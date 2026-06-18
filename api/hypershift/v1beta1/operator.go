11fc118a4e3fd93dce6e5ec7c53fde6af57e4043
Poornima Singour <psingour@redhat.com>
2026-06-18 12:38:41 +0530

    feat(api): add ComponentLogLevelSpec type for per-component log verbosity
    Introduces the ComponentLogLevelSpec struct to support configurable
    log levels on individual hosted control plane components as part of
    OCPSTRAT-3156.
    
    Co-Authored-By: Poornima Singour <psingour@redhat.com>

--- api/hypershift/v1beta1/operator.go
+++ api/hypershift/v1beta1/operator.go
@@ -40,6 +40,18 @@ var (
 	TraceAll LogLevel = "TraceAll"
 )
 
+// ComponentLogLevelSpec configures the log verbosity for a hosted control plane component.
+type ComponentLogLevelSpec struct {
+	// logLevel sets the log verbosity for the component.
+	// Valid values are: "Normal", "Debug", "Trace", "TraceAll".
+	// Defaults to "Normal".
+	//
+	// +optional
+	// +kubebuilder:default=Normal
+       // +kubebuilder:validation:Enum="";Normal;Debug;Trace;TraceAll
+	LogLevel LogLevel `json:"logLevel,omitempty"`
+}
+
 // ClusterVersionOperatorSpec is the specification of the desired behavior of the Cluster Version Operator.
 type ClusterVersionOperatorSpec struct {
 	// operatorLogLevel is an intent based logging for the operator itself. It does not give fine-grained control,
