f52d8495f5efa56b0a8ab6838a3dae5e6e3baeca
vismishr <vismishr@redhat.com>
2026-07-27 11:07:16 +0530

    feat(oauth): OCPSTRAT-3156 Wire ComponentLogLevelSpec for oauth-server
    Add resolveOAuthVerbosity() function that reads the log level from
    OperatorConfiguration.OAuthServer.LogLevel and falls back to default
    verbosity 2. The resolved verbosity is always passed as --v=N to
    the oauth-server container args.

--- control-plane-operator/controllers/hostedcontrolplane/v2/oauth/deployment_test.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/oauth/deployment_test.go
@@ -116,3 +116,85 @@ func TestAdaptDeployment(t *testing.T) {
 		})
 	}
 }
+
+func TestResolveOAuthVerbosity(t *testing.T) {
+	logLevel := func(l hyperv1.LogLevel) hyperv1.OAuthServerOperatorSpec {
+		return hyperv1.OAuthServerOperatorSpec{
+			ComponentLogLevelSpec: hyperv1.ComponentLogLevelSpec{LogLevel: &l},
+		}
+	}
+
+	tests := []struct {
+		name     string
+		hcp      *hyperv1.HostedControlPlane
+		expected int
+	}{
+		{
+			name: "When no operatorConfiguration is set it should default to verbosity 2",
+			hcp: &hyperv1.HostedControlPlane{
+				Spec: hyperv1.HostedControlPlaneSpec{},
+			},
+			expected: 2,
+		},
+		{
+			name: "When operatorConfiguration exists but logLevel is nil it should default to verbosity 2",
+			hcp: &hyperv1.HostedControlPlane{
+				Spec: hyperv1.HostedControlPlaneSpec{
+					OperatorConfiguration: &hyperv1.OperatorConfiguration{},
+				},
+			},
+			expected: 2,
+		},
+		{
+			name: "When oauthServer logLevel is Normal it should return verbosity 2",
+			hcp: &hyperv1.HostedControlPlane{
+				Spec: hyperv1.HostedControlPlaneSpec{
+					OperatorConfiguration: &hyperv1.OperatorConfiguration{
+						OAuthServer: logLevel(hyperv1.Normal),
+					},
+				},
+			},
+			expected: 2,
+		},
+		{
+			name: "When oauthServer logLevel is Debug it should return verbosity 4",
+			hcp: &hyperv1.HostedControlPlane{
+				Spec: hyperv1.HostedControlPlaneSpec{
+					OperatorConfiguration: &hyperv1.OperatorConfiguration{
+						OAuthServer: logLevel(hyperv1.Debug),
+					},
+				},
+			},
+			expected: 4,
+		},
+		{
+			name: "When oauthServer logLevel is Trace it should return verbosity 6",
+			hcp: &hyperv1.HostedControlPlane{
+				Spec: hyperv1.HostedControlPlaneSpec{
+					OperatorConfiguration: &hyperv1.OperatorConfiguration{
+						OAuthServer: logLevel(hyperv1.Trace),
+					},
+				},
+			},
+			expected: 6,
+		},
+		{
+			name: "When oauthServer logLevel is TraceAll it should return verbosity 8",
+			hcp: &hyperv1.HostedControlPlane{
+				Spec: hyperv1.HostedControlPlaneSpec{
+					OperatorConfiguration: &hyperv1.OperatorConfiguration{
+						OAuthServer: logLevel(hyperv1.TraceAll),
+					},
+				},
+			},
+			expected: 8,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewWithT(t)
+			g.Expect(resolveOAuthVerbosity(tt.hcp)).To(Equal(tt.expected))
+		})
+	}
+}
